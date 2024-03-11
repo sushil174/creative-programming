25040059971b69343e3bc55cfbfbdd0d93c77699
sushil174 <sushilkoregave123@gmail.com>
2024-03-11 13:37:46 -0400

    .

---
+++ Perlin-Noise/PerlinNoise.py
@@ -0,0 +1,45 @@
+import pygame
+from sys import exit
+import random
+from perlin_noise import PerlinNoise
+
+def map_range(value, start1, stop1, start2, stop2):
+   return (value - start1) / (stop1 - start1) * (stop2 - start2) + start2
+pygame.init()
+
+screen = pygame.display.set_mode((600,600))
+display = pygame.Surface((200,200))
+clock = pygame.time.Clock()
+noise = PerlinNoise()
+xoff = 0
+yoff = 0
+inc = 0.01
+while True:
+    display.fill('black')
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT :
+            pygame.quit()
+            exit()
+
+    for y in range(0,200):
+        xoff = 0
+        for x in range(0,200):
+            r = noise([xoff,yoff]) * 255
+            #r = map_range(noise([xoff,yoff]),0,1,100,255) 
+            #r = random.randint(0,255)
+            if r < 0: r = r * -1
+            pygame.draw.circle(display,(r,r,r),(x,y),1)
+            xoff += inc
+        yoff += inc
+
+
+    # x = map_range(noise(xoff1),0,1,200,400)
+    # y = map_range(noise(xoff2),0,1,200,400)
+
+    # xoff1 += 0.1
+    # xoff2 += 0.1
+    # #x = random.randint(0,400)
+    # pygame.draw.circle(screen,'white',(x,y),14)
+    screen.blit(pygame.transform.scale(display,screen.get_size()),(0,0))
+    pygame.display.update()
+    clock.tick(30)
---
+++ Random-Walker/RandomWalkV.py
@@ -0,0 +1,30 @@
+import pygame
+from sys import exit
+import random
+
+
+class randomwalk :
+    def __init__(self,x,y):
+        self.pos = pygame.math.Vector2(x,y)
+
+    def update(self):
+        self.pos.x = self.pos.x + random.choice([-1,1])
+        self.pos.y = self.pos.y + random.choice([-1,1])
+
+    def show(self,surface):
+        pygame.draw.circle(surface,'white',(self.pos.x,self.pos.y),1)
+
+screen = pygame.display.set_mode((400,400))
+display = pygame.Surface((200,200))
+clock = pygame.time.Clock()
+walker = randomwalk(100,100)
+while True :
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    walker.update()
+    walker.show(display)
+    pygame.display.update()
+    screen.blit(pygame.transform.scale(display,screen.get_size()),(0,0))
+    clock.tick(30)
