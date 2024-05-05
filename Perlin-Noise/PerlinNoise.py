b6aeceee65551b33362e1c8d6714cf4c61e96698
sushil174 <sushilkoregave123@gmail.com>
2024-05-05 07:33:40 -0400

    changes

--- Perlin-Noise/PerlinNoise.py
+++ Perlin-Noise/PerlinNoise.py
@@ -7,6 +7,8 @@ def map_range(value, start1, stop1, start2, stop2):
    return (value - start1) / (stop1 - start1) * (stop2 - start2) + start2
 pygame.init()
 
+width = 600
+height = 600
 screen = pygame.display.set_mode((600,600))
 display = pygame.Surface((200,200))
 clock = pygame.time.Clock()
@@ -22,21 +24,21 @@ while True:
             pygame.quit()
             exit()
 
-    for y in range(0,200):
+    for y in range(height):
         xoff = 0
-        for x in range(0,200):
+        for x in range(width):
             r = noise([xoff,yoff]) 
             #r = map_range(noise([xoff,yoff]),0,1,0,255) 
             #r = random.randint(0,255)
             # if r < 0: r = 0
 
             if r < 0 :
-                c = 'red'
+                c = (242, 73, 172)
             else :
-                c = 'blue'
+                c = (141, 43, 240)
            
 
-            pygame.draw.circle(display,c,(x,y),1)
+            pygame.draw.circle(screen,c,(x,y),1)
             xoff += inc
         yoff += inc
 
@@ -48,6 +50,6 @@ while True:
     # xoff2 += 0.1
     # #x = random.randint(0,400)
     # pygame.draw.circle(screen,'white',(x,y),14)
-    screen.blit(pygame.transform.scale(display,screen.get_size()),(0,0))
+    #screen.blit(pygame.transform.scale(display,screen.get_size()),(0,0))
     pygame.display.update()
-    clock.tick(30)
+    clock.tick(10)
